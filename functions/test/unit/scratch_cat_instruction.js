d1601cb2c1d868a6782e99d5d14aaa95796be79c
Tina Quach <quacht@mit.edu>
2018-03-11 15:48:24 -0400

    add tests for scratch cat instruction

--- functions/test/unit/scratch_cat_instruction.js
+++ functions/test/unit/scratch_cat_instruction.js
@@ -1,5 +1,5 @@
 const test = require('tap').test;
-const ScratchCat = require('../../scratch_cat_model.js');
+const Instruction = require('../../scratch_cat_instruction.js');
 
 /**
  * Deeply compare the two given arrays.
@@ -31,19 +31,9 @@ var arraysEqual = function(array1, array2) {
     return true;
 };
 
-test('getAvailableActions', t => {
-	var actions = {
-		"a1": [["cmd1", "arg1"],["cmd2", "arg2"]],
-		"a2": [["cmd1", "arg1"],["cmd2", "arg2"]]
-	};
-    var scratch = new ScratchCat(actions);
-    t.same(scratch.getAvailableActions(), ["a1","a2"]);
-	t.end();
-});
-
 // TODO: correct test
 test('extractArgs_ ""', t => {
-    var scratch = new ScratchCat();
+    var scratch = new Instruction("");
     let parsed = scratch.extractArgs_("");
     t.same(parsed, {"original": ""});
 	t.end();
@@ -51,7 +41,7 @@ test('extractArgs_ ""', t => {
 
 test('extractArgs_ "when EVENT, you CMD"', t => {
 	let instruction = "when i say knock knock, you say who's there";
-	var scratch = new ScratchCat();
+	var scratch = new Instruction(instruction);
     var jsonInfo = scratch.extractArgs_(instruction);
 	t.same(jsonInfo, {
 		original: "when i say knock knock, you say who's there",
@@ -63,7 +53,7 @@ test('extractArgs_ "when EVENT, you CMD"', t => {
 
 test('extractArgs_ "when EVENT, CMD"', t => {
 	let instruction = "when i say knock knock, say who's there";
-	var scratch = new ScratchCat();
+	var scratch = new Instruction(instruction);
     var jsonInfo = scratch.extractArgs_(instruction);
 	t.same(jsonInfo, {
 		original: "when i say knock knock, say who's there",
@@ -75,7 +65,7 @@ test('extractArgs_ "when EVENT, CMD"', t => {
 
 test('extractArgs_ identifies "CMD" structure', t => {
 	let instruction = "say who's there";
-	var scratch = new ScratchCat();
+	var scratch = new Instruction(instruction);
     var jsonInfo = scratch.extractArgs_(instruction);
     t.same(jsonInfo.command, instruction);
 	t.end();
@@ -83,7 +73,7 @@ test('extractArgs_ identifies "CMD" structure', t => {
 
 test('extractArgs_ "then/next/after, CMD"', t => {
 	let instruction = "then, say who's there";
-	var scratch = new ScratchCat();
+	var scratch = new Instruction(instruction);
     var jsonInfo = scratch.extractArgs_(instruction);
 	t.same(jsonInfo, {
 		original: instruction,
@@ -95,7 +85,7 @@ test('extractArgs_ "then/next/after, CMD"', t => {
 
 test('extractArgs_ "first, CMD"', t => {
 	let instruction = "first, say who's there";
-	var scratch = new ScratchCat();
+	var scratch = new Instruction(instruction);
     var jsonInfo = scratch.extractArgs_(instruction);
 	t.same(jsonInfo, {
 		original: instruction,
@@ -106,74 +96,25 @@ test('extractArgs_ "first, CMD"', t => {
 });
 
 test('getSteps works with single instruction', t => {
-	var scratch = new ScratchCat();
 	let instruction = 'say King Tut-key fried chicken!';
+	var scratch = new Instruction(instruction);
 	var step = scratch.getSteps(instruction);
 	t.same(step, [['say:', 'King Tut-key fried chicken!']]);
 	t.end();
 });
 
 test('getSteps works with single instruction with event', t => {
-	var scratch = new ScratchCat();
 	let instruction = "when I say knock knock, say who's there?";
+	var scratch = new Instruction(instruction);
 	let step = scratch.getSteps(instruction);
 	arraysEqual(step, [["doAsk", ""],["doIf", ["=", ["answer"], "knock knock"], [["say:", "who's there?"]]]]);
 	t.end();
 });
 
 test('getSteps works with multiple instructions in single utterance', t => {
-	var scratch = new ScratchCat();
 	let instruction = "First, say knock knock. Then, when I say who's there, you say King Tut";
+	var scratch = new Instruction(instruction);
 	let step = scratch.getSteps(instruction);
 	arraysEqual(step, [["say:", "knock knock"],["doAsk", ""],["doIf", ["=", ["answer"], "who's there"], [["say:", "King Tut"]]]]);
-	t.end();
-});
-
-
-test('createProgram returns valid program', t => {
-    var scratch = new ScratchCat();
-    let steps = [["doAsk", ""],["doIf", ["=", ["answer"], "knock knock"], [["say:", "who's there?"]]]];
-    let prog = scratch.createProgram(steps);
-    t.same(prog, [["whenGreenFlag"],
-						["doAsk", ""],
-						["doIf", ["=", ["answer"], "knock knock"], [["say:", "who's there?"]]]])
-	t.end();
-});
-
-test('addAction', t => {
-    var scratch = new ScratchCat();
- 	let actionName = 'actionName';
- 	let steps = [["cmd", "arg"],[ "cmd1", "arg1"]]
-    scratch.addAction(actionName, steps)
-    t.ok(actionName in scratch.actions);
-    t.same(scratch.actions['actionName'], [["cmd", "arg"],[ "cmd1", "arg1"]]);
-	t.end();
-});
-
-test('appendSteps', t => {
- 	let actions = {'actionName': [["cmd", "arg"],[ "cmd1", "arg1"]]}
- 	var scratch = new ScratchCat(actions);
- 	let steps = [["cmd3", "arg3"],[ "cmd4", "arg4"]]
-    scratch.appendSteps('actionName', steps)
-    t.ok('actionName' in scratch.actions);
-    t.same(scratch.actions['actionName'], [["cmd", "arg"],[ "cmd1", "arg1"], ["cmd3", "arg3"],[ "cmd4", "arg4"]]);
-	t.end();
-});
-
-test('appendSteps on empty step list', t => {
- 	let actions = {'actionName': [["cmd", "arg"],[ "cmd1", "arg1"]]}
- 	var scratch = new ScratchCat(actions);
- 	let steps = []
-    scratch.appendSteps('actionName', steps)
-    t.ok('actionName' in scratch.actions);
-    t.same(scratch.actions['actionName'], [["cmd", "arg"],[ "cmd1", "arg1"]]);
-	t.end();
-});
-
-test('replaceStep', t => {
-  	let actions = {'actionName': [["cmd", "arg"],[ "cmd1", "arg1"]]}
- 	var scratch = new ScratchCat(actions);
- 	scratch.replaceStep('actionName', 1, ["cmd_replace", "arg_replace"]);
- 	t.same(scratch.actions['actionName'], [["cmd", "arg"],["cmd_replace", "arg_replace"]]);
 	t.end();
 });
