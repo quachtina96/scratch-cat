91e77903e29b40266d9232ab9d576a033f9101a5
Tina Quach <quacht@mit.edu>
2018-03-11 14:26:35 -0400

    Add ScratchCatInstruction Class and Tests

---
+++ functions/scratch_cat_instruction.js
@@ -0,0 +1,114 @@
+/**
+ * @fileoverview Define ScratchCatInstruction class for converting sentences
+ * to Scratch programs
+ * @author Tina Quach (quacht@mit.edu)
+ */
+
+class ScratchCatInstruction {
+  constructor(rawInstruction) {
+    this.raw = rawInstruction;
+    this.steps = this.getSteps(rawInstruction);
+  }
+
+  /**
+   * Returns the steps of the Scratch program.
+   */
+  getSteps(instruction) {
+    //TODO: detect multiple statements and split them.
+    let sentences = instruction.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+    var steps = []
+    for (var i = 0; i < sentences.length; i++) {
+      var instructionJson = this.extractArgs_(sentences[i]);
+      var scratchInstruction = this.jsonToScratch_(instructionJson);
+      steps.push(scratchInstruction);
+    }
+    return steps;
+  }
+
+  /**
+   * Helper function for getSteps. Returns a JSON representing the instruction.
+   * @param {!String} instruction A sentence that may contain a command.
+   * @return JSON object encoding information for generating Scratch program.
+   */
+  extractArgs_(instruction) {
+    var instructionJson = {
+      original: instruction
+    };
+
+    if (!instruction) {
+      return instructionJson;
+    }
+
+    let commandTemplates = {
+      'when EVENT, you CMD': /[wW]hen (.*), you (.*)/,
+      'when EVENT, CMD': /[wW]hen (.*), (.*)/,
+      'then/next/after, CMD': /([Tt]hen|[nN]ext|[aA]fter), (.*)/,
+      'first, CMD': /[fF]irst, (.*)/,
+    };
+    var keys = Object.keys(commandTemplates);
+
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      var matches = instruction.match(commandTemplates[key]);
+      if (matches) {
+        switch (key) {
+          case 'when EVENT, you CMD':
+          case 'when EVENT, CMD':
+            instructionJson.event = matches[1];
+            instructionJson.command = matches[2];
+            return instructionJson;
+          case 'then/next/after, CMD':
+            instructionJson.event = 'after last command';
+            instructionJson.command = matches[2];
+            return instructionJson;
+          case 'first, CMD':
+            instructionJson.event = 'first';
+            instructionJson.command = matches[1];
+            return instructionJson;
+        }
+      }
+    }
+
+    instructionJson.command = instruction;
+
+    return instructionJson;
+  }
+
+  /**
+   * Helper function for getSteps. Returns a JSON representing the instruction.
+   * @param {!String} instruction A sentence that may contain a command.
+   */
+  jsonToScratch_(instructionJson) {
+    if (!instructionJson.command) {
+      return null;
+    }
+    // Process command.
+    var instructionTokens = instructionJson.command.split(' ');
+    if (instructionTokens) {
+      // Assume first word is the verb and the rest of the command is an
+      // argument.
+      var verb = instructionTokens[0];
+      // TODO: add more cases to handle wider variety of possible scratch
+      // commands.
+      if (verb === 'say') {
+        var opcode = 'say:';
+      }
+      var command = [opcode, instructionTokens.slice(1).join(' ')];
+    }
+
+    if (instructionJson.event) {
+      if (instructionJson.event.toLowerCase().startsWith('i say')) {
+        let argument = instructionJson.event.substring(5);
+        command = [["doAsk", ""],["doIf", ["=", ["answer"], argument], [command]]];
+      }
+      // else if (instructionJson.event == 'first') {
+      //   // TODO: Need some way to pass this information to Scratch model for building the progam.
+      // } else if (instructionJson.event == 'after last command') {
+      //   // TODO: Need some way to pass this information to Scratch model for building the progam.
+      // }
+    }
+    return command;
+  }
+}
+
+module.exports = ScratchCatInstruction;
--- functions/scratch_cat_model.js
+++ functions/scratch_cat_model.js
@@ -2,6 +2,8 @@
  * This file describes the ScratchCatModel class for the voice user interface for
  * programming.
  */
+'use strict';
+const ScratchCatInstruction = require('./scratch_cat_instruction.js');
 
 class ScratchCatModel {
   constructor (opt_availableActions) {
@@ -20,141 +22,23 @@ class ScratchCatModel {
     return action in this.actions;
   }
 
-  // TODO: move getSteps and extractArgs_ into a ScratchCatInstruction class?
-  // extractArgs_ would be included in the initialization and would provide a
-  // nice API for interacting with the arguments.
-  /**
-   * Helper function for getSteps. Returns a JSON representing the instruction.
-   * @param {!String} instruction A sentence that may contain a command.
-   */
-  extractArgs_(instruction) {
-    var instructionJson = {
-      original: instruction
-    };
-
-    if (!instruction) {
-      return instructionJson;
-    }
-
-    let commandTemplates = {
-      'when EVENT, you CMD': /when (.*), you (.*)/,
-      'when EVENT, CMD': /when (.*), (.*)/,
-      'then/next/after, CMD': /(then|next|after), (.*)/,
-      'first, CMD': /first, (.*)/,
-    };
-    var keys = Object.keys(commandTemplates);
-
-    for (var i = 0; i < keys.length; i++) {
-      var key = keys[i];
-      var matches = instruction.match(commandTemplates[key]);
-      if (matches) {
-        switch (key) {
-          case 'when EVENT, you CMD':
-          case 'when EVENT, CMD':
-            instructionJson.event = matches[1];
-            instructionJson.command = matches[2];
-            return instructionJson;
-          case 'then/next/after, CMD':
-            instructionJson.event = 'after last command';
-            instructionJson.command = matches[2];
-            return instructionJson;
-          case 'first, CMD':
-            instructionJson.event = 'first';
-            instructionJson.command = matches[1];
-            return instructionJson;
-        }
-      }
-    }
-
-    instructionJson.command = instruction;
-
-    return instructionJson;
-  }
-
-  jsonToScratch_(instructionJson) {
-    if (!instructionJson.command) {
-      return null;
-    }
-    // Process command.
-    var instructionTokens = instructionJson.command.split(' ');
-    if (instructionTokens) {
-      // Assume first word is the verb and the rest of the command is an
-      // argument.
-      var verb = instructionTokens[0];
-      // TODO: add more cases to handle wider variety of possible scratch
-      // commands.
-      if (verb === 'say') {
-        var opcode = 'say:';
-      }
-      var command = [opcode, instructionTokens.slice(1).join(' ')];
-    }
-
-    if (instructionJson.event) {
-      if (instructionJson.event.toLowerCase().startsWith('i say')) {
-        let argument = instructionJson.event.substring(5);
-        command = [["doAsk", ""],["doIf", ["=", ["answer"], argument], [command]]];
-      }
-      // else if (instructionJson.event == 'first') {
-      //   // TODO: Need some way to pass this information to Scratch model for building the progam.
-      // } else if (instructionJson.event == 'after last command') {
-      //   // TODO: Need some way to pass this information to Scratch model for building the progam.
-      // }
-    }
-    return command;
-  }
-
-  /**
-   * Returns the steps of the Scratch program.
-   */
-  getSteps(instruction) {
-    //TODO: detect multiple statements and split them.
-    let sentences = instruction.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
-    var steps = []
-    for (var i = 0; i < sentences.length; i++) {
-      var instructionJson = this.extractArgs_(sentences[i]);
-      var scratchInstruction = this.jsonToScratch_(instructionJson);
-      steps.push(scratchInstruction);
-    }
-    return steps;
-  }
-
-  /**
-   * Everytime you want to execute the program, you add a
-   * when green flag block to start it.
-   */
-  createProgram(steps) {
-    return [['whenGreenFlag']].concat(steps);
-  }
-
   /**
    * Adds a new ability and associates a new program containing given
    * instruction to it.
    * @param {!String} actionName Name of the action
-   * @param {!Array<Array>} step The steps in the program
+   * @param {!String} instruction The first instruction
    **/
-  addAction(actionName, steps) {
-    this.actions[actionName] = steps;
+  addAction(actionName, instruction) {
+    this.actions[actionName] = new ScratchCatInstruction(instruction);
   }
 
   /**
-   * Adds step to program associated with existing ability.
+   * Return the action
    * @param {!String} actionName Name of the action
-   * @param {!Object} step The new step
+   * @param {ScratchCatAction}
    **/
-  appendSteps(actionName, steps) {
-    for (var i = 0; i < steps.length; i++) {
-      this.actions[actionName].push(steps[i]);
-    }
-  }
-
-  /**
-   * Replaces step at given index in program with new step.
-   * @param {!String} actionName
-   * @param {!Integer} index Location of step to replace
-   * @param {!Object} step sequence of functions to evaluate
-   **/
-  replaceStep(actionName, index, step) {
-    this.actions[actionName][index] = step;
+  getAction(actionName) {
+    return this.actions[actionName];
   }
 }
 
